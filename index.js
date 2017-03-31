b612495e3f15f1e259425196ce3e06d6ba1dbdbb
bijuneyyan <bijuneyyan@gmail.com>
2017-03-31 07:39:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,53 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const restService = express();
 
+//biju
+var request = require('request');
+
+//biju
+request.post(
+    'https://www.googleapis.com/calendar/v3/calendars/primary/events/quickAdd',
+    {
+ "kind": "calendar#event",
+ "etag": "\"2981851865534000\"",
+ "id": "d8v82gjhfor9u1mc5vtb6pldns",
+ "status": "confirmed",
+ "htmlLink": "https://www.google.com/calendar/event?eid=ZDh2ODJnamhmb3I5dTFtYzV2dGI2cGxkbnMgZ2tjNnBsYjloZnN1aGwxb3JnczRiOGpvb3NAZw",
+ "created": "2017-03-31T02:05:32.000Z",
+ "updated": "2017-03-31T02:05:32.767Z",
+ "summary": "call john",
+ "creator": {
+  "email": "bijuneyyan@gmail.com",
+  "displayName": "Biju Neyyan"
+ },
+ "organizer": {
+  "email": "gkc6plb9hfsuhl1orgs4b8joos@group.calendar.google.com",
+  "displayName": "botcalendar",
+  "self": true
+ },
+ "start": {
+  "dateTime": "2017-03-27T15:00:00+05:30"
+ },
+ "end": {
+  "dateTime": "2017-03-27T16:00:00+05:30"
+ },
+ "iCalUID": "d8v82gjhfor9u1mc5vtb6pldns@google.com",
+ "sequence": 0,
+ "reminders": {
+  "useDefault": true
+ }
+},
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
+
+
+//originalbelow
 restService.use(bodyParser.urlencoded({
     extended: true
 }));
