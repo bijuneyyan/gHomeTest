4eff8f2a2dc1bfd8901132846382c200ef18be84
bijuneyyan <bijuneyyan@gmail.com>
2017-03-31 07:52:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,44 +8,30 @@ const restService = express();
 var request = require('request');
 
 //biju
-request.post(
-    'https://www.googleapis.com/calendar/v3/calendars/primary/events/quickAdd',
-    {
- "kind": "calendar#event",
- "etag": "\"2981851865534000\"",
- "id": "d8v82gjhfor9u1mc5vtb6pldns",
- "status": "confirmed",
- "htmlLink": "https://www.google.com/calendar/event?eid=ZDh2ODJnamhmb3I5dTFtYzV2dGI2cGxkbnMgZ2tjNnBsYjloZnN1aGwxb3JnczRiOGpvb3NAZw",
- "created": "2017-03-31T02:05:32.000Z",
- "updated": "2017-03-31T02:05:32.767Z",
- "summary": "call john",
- "creator": {
-  "email": "bijuneyyan@gmail.com",
-  "displayName": "Biju Neyyan"
- },
- "organizer": {
-  "email": "gkc6plb9hfsuhl1orgs4b8joos@group.calendar.google.com",
-  "displayName": "botcalendar",
-  "self": true
- },
- "start": {
-  "dateTime": "2017-03-27T15:00:00+05:30"
- },
- "end": {
-  "dateTime": "2017-03-27T16:00:00+05:30"
- },
- "iCalUID": "d8v82gjhfor9u1mc5vtb6pldns@google.com",
- "sequence": 0,
- "reminders": {
-  "useDefault": true
- }
-},
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-        }
-    }
-);
+var http = require("http");
+var options = {
+  hostname: 'www.postcatcher.in',
+  port: 80,
+  path: '/catchers/58ddbd19bbfc780400000002',
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
+  }
+};
+var req = http.request(options, function(res) {
+  console.log('Status: ' + res.statusCode);
+  console.log('Headers: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (body) {
+    console.log('Body: ' + body);
+  });
+});
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+// write data to request body
+req.write('{"string": "Hello, World"}');
+req.end();
 
 
 //originalbelow
