c1b1b9df00ce480130b1dfaaa7908c6582f04b1d
bijuneyyan <bijuneyyan@gmail.com>
2017-04-01 02:39:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
-        speech: speech,
+        speech: "Okay, I will remind biju to" speech,
         displayText: speech,
         source: 'webhook-echo-sample'
     });
